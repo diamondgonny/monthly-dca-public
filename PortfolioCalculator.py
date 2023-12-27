1a905e5a12a48627ff59074600c6bd45e92ee8e2
Kim Keon-Young <diamondgonny@gmail.com>
2023-12-27 12:49:37 +0900

    Update explanation of the file 'PortfolioCalculator.py'

--- PortfolioCalculator.py
+++ PortfolioCalculator.py
@@ -37,7 +37,7 @@ def main():
     check_token_expired()
     spreadsheet_url, _ = path_google_sheet()
 
-    # Input
+    # Login, Input(from Google Sheet)
     worksheet = auth_google_sheet('excel-editor-1.json', spreadsheet_url, '*dashboard')
     g_row_stack_pointer_ki, buyable_cash_won_ki, portfolio_input_ki = get_info_ki_from_google_sheet(worksheet, ROW_BASE_POINTER_KI)
 
@@ -46,7 +46,7 @@ def main():
     portfolio_input_ki = get_qty_from_kis_developers(**portfolio_input_ki)
     buyable_cash_won_ki = get_buyable_cash_from_kis_developers()
 
-    # Output(Google Sheet)
+    # Output(to Google Sheet)
     put_mkt_qty_cash_to_google_sheet(worksheet, ROW_BASE_POINTER_KI, g_row_stack_pointer_ki, buyable_cash_won_ki,
                                      **portfolio_input_ki)
 
@@ -61,7 +61,7 @@ def main():
     g_start_time = time.time()
     spreadsheet_url, _ = path_google_sheet()
 
-    # Input
+    # Login
     book = auth_google_file('excel-editor-1.json', spreadsheet_url)
     existing_sheet_name = '*dashboard'
     to_copy_sheet_name = datetime.datetime.now().strftime('%Y%m%d')  # ex. 20240101
@@ -69,7 +69,7 @@ def main():
     # Process(Google Sheet : *dashboard -> 20240101)
     to_copy_sheet_name, existing_sheet = find_existing_sheet_and_duplicate(existing_sheet_name, to_copy_sheet_name, book)
 
-    # Save(.json)
+    # Backup(to .json)
     backup_exchange_rate(existing_sheet)
     backup_sheet_name(spreadsheet_url, to_copy_sheet_name)
 
@@ -84,7 +84,7 @@ def main():
     check_token_expired()
     spreadsheet_url, sheet_name = path_google_sheet()
 
-    # Input
+    # Login, Input(from Google Sheet)
     worksheet = auth_google_sheet('excel-editor-2.json', spreadsheet_url, sheet_name)
     buyable_cash_dollar_ki, portfolio_input_ki = get_portfolio_from_google_sheet_ki_1(worksheet, ROW_BASE_POINTER_KI,
                                                                                       g_row_stack_pointer_ki)
@@ -100,7 +100,7 @@ def main():
     buyable_cash_dollar_ki, portfolio_dict_ki = estimate_portfolio_and_show_progress_ki(buyable_cash_dollar_ki,
                                                                                         **portfolio_dict_ki)
 
-    # Save(.json)
+    # Save(to .json)
     put_portfolio_to_json_file('ki', buyable_cash_dollar_ki, **portfolio_dict_ki)
 
     g_end_time = time.time()
@@ -114,7 +114,7 @@ def main():
     check_token_expired()
     spreadsheet_url, sheet_name = path_google_sheet()
 
-    # Input
+    # Login, Input(from Google Sheet)
     worksheet = auth_google_sheet('excel-editor-4.json', spreadsheet_url, sheet_name)
     buyable_cash_dollar_nh1, portfolio_input_nh1 = get_portfolio_from_google_sheet_nh('nh1', worksheet, ROW_BASE_POINTER_NH1,
                                                                                       ROW_STACK_POINTER_NH1)
@@ -135,7 +135,7 @@ def main():
     buyable_cash_dollar_nh2, portfolio_dict_nh2 = estimate_portfolio_and_show_progress_nh(buyable_cash_dollar_nh2,
                                                                                           **portfolio_dict_nh2)
 
-    # Save(.json)
+    # Save(to .json)
     put_portfolio_to_json_file('nh1', buyable_cash_dollar_nh1, **portfolio_dict_nh1)
     put_portfolio_to_json_file('nh2', buyable_cash_dollar_nh2, **portfolio_dict_nh2)
     print()
@@ -151,7 +151,7 @@ def main():
     check_token_expired()
     spreadsheet_url, sheet_name = path_google_sheet()
 
-    # Load(.json)
+    # Login, Load(from .json)
     worksheet = auth_google_sheet('excel-editor-1.json', spreadsheet_url, sheet_name)
     portfolio_output_nh1 = open_json_file('./output/portfolio_output_nh1.json')
     portfolio_output_nh2 = open_json_file('./output/portfolio_output_nh2.json')
@@ -172,7 +172,7 @@ def main():
 
     portfolio_dict_nh1 = {'BRK.B' if ticker == 'BRK/B' else ticker: list for ticker, list in portfolio_dict_nh1.items()}
 
-    # Output(Google Sheet)
+    # Output(to Google Sheet)
     put_target_qty_to_google_sheet(worksheet, 'nh1', ROW_BASE_POINTER_NH1, ROW_STACK_POINTER_NH1, **portfolio_dict_nh1)
     put_target_qty_to_google_sheet(worksheet, 'nh2', ROW_BASE_POINTER_NH2, ROW_STACK_POINTER_NH2, **portfolio_dict_nh2)
     put_target_qty_to_google_sheet(worksheet, 'ki', ROW_BASE_POINTER_KI, g_row_stack_pointer_ki, **portfolio_dict_ki)
